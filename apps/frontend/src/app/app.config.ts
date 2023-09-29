7e7583c2ec753ba6114c19490b4fcee70c06e2bf
Kevin Bonnoron <2421321+KevinBonnoron@users.noreply.github.com>
2023-09-29 06:18:42 +0000

    feat: step 1

--- apps/frontend/src/app/app.config.ts
+++ apps/frontend/src/app/app.config.ts
@@ -1,18 +1,27 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
 } from '@angular/router';
+import { provideEffects } from '@ngrx/effects';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideEffects(),
+    provideStore(),
+    provideStoreDevtools({ logOnly: !isDevMode() }),
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
     provideAnimations(),
     provideHttpClient(),
-    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } },
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: 'outline' },
+    },
   ],
 };
--- libs/products/data-access/src/lib/products-data-access.module.ts
+++ libs/products/data-access/src/lib/products-data-access.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
+import { provideEffects } from '@ngrx/effects';
+import { provideState } from '@ngrx/store';
+import { ProductsEffects } from './+state/products.effects';
+import { ProductsFacade } from './+state/products.facade';
+import * as fromProducts from './+state/products.reducer';
 import { ProductsService } from './services';
 
 @NgModule({
-  providers: [ProductsService]
+  providers: [
+    provideState(
+      fromProducts.PRODUCTS_FEATURE_KEY,
+      fromProducts.productsReducer
+    ),
+    provideEffects([ProductsEffects]),
+    ProductsService,
+    ProductsFacade
+  ],
 })
 export class ProductsDataAccessModule {}
